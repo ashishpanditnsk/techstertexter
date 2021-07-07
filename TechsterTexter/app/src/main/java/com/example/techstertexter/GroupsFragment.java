529e2d13b77ee6210af19f0b298405cc86307c36
Varnika <200020157@iitb.ac.in>
2021-07-07 20:41:16 +0530

    Authentication Added

---
+++ TechsterTexter/app/src/main/java/com/example/techstertexter/GroupsFragment.java
@@ -0,0 +1,42 @@
+package com.example.techstertexter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class GroupsFragment extends Fragment
+{
+
+    public GroupsFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+
+        return  inflater.inflate(R.layout.fragment_groups, container, false);
+    }
+
+
+}
---
+++ TechsterTexter/app/src/main/java/com/example/techstertexter/TabsAccessorAdapter.java
@@ -0,0 +1,63 @@
+package com.example.techstertexter;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdapter extends FragmentPagerAdapter {
+
+    public TabsAccessorAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i)
+        {
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+
+            case 1:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position){
+        if (position == 0) {
+            return "Chats";
+        }
+        if (position == 1) {
+            return "Groups";
+        }
+        if (position == 2) {
+            return "Contacts";
+        }
+        if (position == 3) {
+            return "Requests";
+        }
+        else{
+            return null;
+        }
+    }
+}
